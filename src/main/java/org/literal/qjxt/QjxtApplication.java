400ae0576f2edaf697a792b2df02a3ed4776df3e
CSH <2389579869@qq.com>
2021-09-27 16:07:16 +0800

    Initial commit
    create project
    config spring data source

---
+++ src/main/java/org/literal/qjxt/QjxtApplication.java
@@ -0,0 +1,17 @@
+package org.literal.qjxt;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// alt + / 代码提示
+
+@SpringBootApplication
+@MapperScan("org.literal.qjxt.dao")
+public class QjxtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QjxtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/literal/QjxtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.literal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QjxtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
